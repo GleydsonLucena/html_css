ae53c6e9e3b4dc2ff5fb9834d1b879059d54034b
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-02-01 18:25:02 -0300

    Atualização

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -1,1 +1,12 @@
-alert('opa')
+const menu = document.getElementById('menu-burguer')
+const navbar = document.getElementById('navbar')
+
+menu.addEventListener('click', click())
+
+function click() {
+    if (navbar.style.display == 'flex') {
+        navbar.style.display = 'none'
+    } else {
+        navbar.style.display = 'flex'
+    }
+}
